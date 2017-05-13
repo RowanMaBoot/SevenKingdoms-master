27201550e0b7790770197f96195f63e05cebc61a
Altemio <nobody@rccclan.com>
2017-05-13 18:25:55 -0500

    More Tweaks
    Because I felt like it, I guess.

--- The_Seven_Kingdoms/common/defines/00_defined_tweaks.lua
+++ The_Seven_Kingdoms/common/defines/00_defined_tweaks.lua
@@ -2,6 +2,23 @@
 --
 -- NDefines.NDiplomacy.DEMESNE_BASE_MAX_SIZE = 2.0
 
+-- Diplomacy
+NDefines.NDiplomacy.VASSAL_LIMIT_LEVY_MULTIPLIER = 0
+NDefines.NDiplomacy.LAW_CHANGE_PRESTIGE_COST = 150 -- from 100
+NDefines.NDiplomacy.CROWN_LAW_CHANGE_PRESTIGE_COST = 300 -- from 200
+NDefines.NDiplomacy.BASE_REVOLT_CHANCE_MOD = 200 -- from 250
+NDefines.NDiplomacy.DUKE_POWERFUL_VASSAL_COUNT = 3 -- from 4
+NDefines.NDiplomacy.KING_POWERFUL_VASSAL_COUNT = 4 -- from 5
+NDefines.NDiplomacy.EMPEROR_POWERFUL_VASSAL_COUNT = 5 -- from 6
+NDefines.NDiplomacy.IMPRISON_CHARACTER_INTERACTION_PIETY = 0 -- Reduced from 10
+NDefines.NDiplomacy.EXECUTE_IMPRISONED_INTERACTION_PIETY = 10 -- Reduced from 20
+NDefines.NDiplomacy.DEMAND_RELIGIOUS_CONVERSION_INTERACTION_PIETY = 25 -- Increased from 0
+NDefines.NDiplomacy.DEMAND_RELIGIOUS_CONVERSION_INTERACTION_PRESTIGE = 50 -- Increased from 0
+
+-- Council
+NDefines.NCouncil.ENFORCE_PEACE_START_DELAY = 6 -- from 3
+NDefines.NCouncil.LAW_VOTE_CHANGE_TIME_LIMIT = 2 -- from 1
+
 -- Titles
 NDefines.NTitle.BARON_LANDLESS_SON_PRESTIGE = 0.005
 NDefines.NTitle.COUNT_LANDLESS_SON_PRESTIGE = 0.05
@@ -10,8 +27,17 @@ NDefines.NTitle.KING_LANDLESS_SON_PRESTIGE = 0.2
 NDefines.NTitle.EMPEROR_LANDLESS_SON_PRESTIGE = 0.4
 
 -- Characters
-NDefines.NCharacter.PORTRAIT_ADULT_MALE_AGE_THRESHOLD = 15
-NDefines.NCharacter.PORTRAIT_ADULT_FEMALE_AGE_THRESHOLD = 15
+
+NDefines.NCharacter.CHANGE_AMBITION_YEARS = 1 -- from 3
+NDefines.NCharacter.CHANGE_FOCUS_YEARS = 3 -- from 5
+
+NDefines.NCharacter.MAX_JOINED_FACTIONS = 4
+
+NDefines.NCharacter.PRESTIGE_FROM_DYNASTY_ON_BIRTH_DIV = 15
+NDefines.NCharacter.PRESTIGE_FROM_DYNASTY_ON_MARRIAGE_DIV = 15
+
+NDefines.NCharacter.PORTRAIT_ADULT_MALE_AGE_THRESHOLD = 14
+NDefines.NCharacter.PORTRAIT_ADULT_FEMALE_AGE_THRESHOLD = 14
 
 NDefines.NCharacter.PORTRAIT_MID_AGE_THRESHOLD = 30
 NDefines.NCharacter.PORTRAIT_OLD_AGE_THRESHOLD = 50
@@ -42,8 +68,19 @@ NDefines.NCharacter.FEMALE_ATTRACTION_CUTOFF = 48
 -- Religion
 NDefines.NReligion.AUTHORITY_FROM_ORG_RELIGION = 0.3
 
+-- Economy
+NDefines.NEconomy.BISHOP_TAX_TO_POPE_FACTOR = 0.15 -- from 0.1
+NDefines.NEconomy.TRADE_POST_COST_INC_DIST = 0.0045
+NDefines.NEconomy.OVER_MAX_DEMESNE_TAX_PENALTY = 0.05 -- reduced penalty from 20%
+
 -- Military
+
+NDefines.NMilitary.MAX_COMMANDERS_DUKE = 5 -- from 4
+NDefines.NMilitary.MAX_COMMANDERS_KING = 7 -- from 6
+NDefines.NMilitary.MAX_COMMANDERS_EMPEROR = 9 -- from 8
+
 NDefines.NMilitary.FRIENDLY_TERRITORY_LEVY_RETURN_PERCENT = 0.9 -- Reduced from 1.0 to reduce exploiting.
+NDefines.NMilitary.OTHER_TERRITORY_LEVY_RETURN_PERCENT = 0.33 -- Reduced from 0.50 (50% loss) to 0.33 (77% loss) to reduce exploiting.
 
 NDefines.NMilitary.BATTLE_TECH_MULTIPLIER = 0.0 -- Removed
 
@@ -60,6 +97,8 @@ NDefines.NMilitary.LEVY_MAINTENANCE_FACTOR = 1.5
 
 NDefines.NMilitary.LIEGE_LEVY_REINF_RATE = 0.03 -- Reduced for 0.05
 NDefines.NMilitary.LIEGE_LEVY_COST_MULTIPLIER = 0.5 -- Halved from 1.0
+
+NDefines.NMilitary.MIN_LEVY_RAISE_OPINION_THRESHOLD = -33 -- Lowered from 0
 
 NDefines.NMilitary.LIGHT_INFANTRY_MORALE = 2
 NDefines.NMilitary.LIGHT_INFANTRY_MAINTENANCE = 0.70
@@ -132,6 +171,10 @@ NDefines.NTechnology.BASE_TO_NOMAD_CAPITAL_SPREAD_BONUS = 0.0
 
 -- Graphics
 NDefines.NGraphics.CITY_SPRAWL_AMOUNT = 1.4
+
+-- AI
+NDefines.NAI.RAID_MAX_REALM_SIZE = 48 -- from 24
+NDefines.NAI.RAID_AGGRESSION = 20 -- from 18, slightly fewer raids
 
 -- Ruler Designer
 NDefines.NRulerDesigner.MAX_AGE = 70
