1c7fc6c8544008fdafec3cef49b8671f9aa5fc15
Vierwood <ethaneeh@hotmail.com>
2017-09-11 19:52:30 -0500

    Camera
    Needs some working with

---
+++ The_Seven_Kingdoms/common/defines/01_camera_reposition.lua
@@ -0,0 +1,52 @@
+NFrontend = 
+{
+	CAMERA_LOOKAT_X = 790.0, 			-- Rotation point in main menu
+	CAMERA_LOOKAT_Y = 0.0,
+	CAMERA_LOOKAT_Z = 1000.0,
+	CAMERA_LOOKAT_SETTINGS_X = 1400.0,  -- Rotation point in settings
+	CAMERA_LOOKAT_SETTINGS_Y = 0.0,		-- Y is height
+	CAMERA_LOOKAT_SETTINGS_Z = 648.0,
+	CAMERA_START_X = 790.0,				-- Initial position in main menu
+	CAMERA_START_Y = 150.0,				-- Y is height
+	CAMERA_START_Z = 500.0,
+	CAMERA_END_X = 790.0,				-- Move to position in main menu
+	CAMERA_END_Y = 500.0,
+	CAMERA_END_Z = 600.0,
+	TIME_FROZEN = 1.0,  				-- Time before initial animation starts (some delay here so it should NOT be 0, then the animation starts before you can see it)
+	GUI_MOVE_SPEED = 800,				-- How fast sliding gui objects move ( pixels/s )
+	MAX_ZOOM_LEVEL = 2500,				-- How far out the player can zoom. Warning: At very high zoom levels, the ground will no longer render
+	
+	FADE_IN_DONE_TIME = 2.2,
+	GUI_START_MOVE_TIME = 1.4,
+	CAMERA_START_MOVE_TIME = 3.0,
+	
+	CAMERA_SPEED_IN_MENUS = 0.1,
+	
+	FRONTEND_POS_X = 790.0,
+	FRONTEND_POS_Y = 500.0,
+	FRONTEND_POS_Z = 600.0,
+	FRONTEND_LOOK_X = 790.0,
+	FRONTEND_LOOK_Y = 0.0,
+	FRONTEND_LOOK_Z = 1000.0,
+	
+	SETTINGS_POS_X = 790.0,
+	SETTINGS_POS_Y = 500.0,
+	SETTINGS_POS_Z = 400.0,
+	SETTINGS_LOOK_X = 1400.0,
+	SETTINGS_LOOK_Y = 0.0,		
+	SETTINGS_LOOK_Z = 648.0,
+	
+	MP_OPTIONS_POS_X = 360.0,
+	MP_OPTIONS_POS_Y = 200.0,
+	MP_OPTIONS_POS_Z = 848.0,
+	MP_OPTIONS_LOOK_X = 490.0,
+	MP_OPTIONS_LOOK_Y = 0.0,	
+	MP_OPTIONS_LOOK_Z = 1200.0,
+	
+	CREDITS_POS_X = 1000.0,
+	CREDITS_POS_Y = 200.0,
+	CREDITS_POS_Z = 1308.0,
+	CREDITS_LOOK_X = 900.0,
+	CREDITS_LOOK_Y = 0.0,	
+	CREDITS_LOOK_Z = 1450.0,
+},
