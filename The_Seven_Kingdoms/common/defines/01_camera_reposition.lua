e15594ac4713ce7e6a89e9330bfb3f5dc6608953
Vierwood <ethaneeh@hotmail.com>
2017-09-11 20:12:46 -0500

    New sexy camera
    Will tweak more later

--- The_Seven_Kingdoms/common/defines/01_camera_reposition.lua
+++ The_Seven_Kingdoms/common/defines/01_camera_reposition.lua
@@ -1,13 +1,13 @@
-NDefines.NFrontend.CAMERA_LOOKAT_X = 790.0 -- Rotation point in main menu
+NDefines.NFrontend.CAMERA_LOOKAT_X = 1450.0 -- Rotation point in main menu
 NDefines.NFrontend.CAMERA_LOOKAT_Y = 0.0
-NDefines.NFrontend.CAMERA_LOOKAT_Z = 1000.0
+NDefines.NFrontend.CAMERA_LOOKAT_Z = 1200.0
 NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_X = 1400.0 -- Rotation point in settings
 NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Y = 0.0 -- Y is height
 NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Z = 648.0
 NDefines.NFrontend.CAMERA_START_X = 790.0 -- Initial position in main menu
 NDefines.NFrontend.CAMERA_START_Y = 150.0 -- Y is height
 NDefines.NFrontend.CAMERA_START_Z = 500.0
-NDefines.NFrontend.CAMERA_END_X = 790.0 -- Move to position in main menu
+NDefines.NFrontend.CAMERA_END_X = 1450.0 -- Move to position in main menu
 NDefines.NFrontend.CAMERA_END_Y = 500.0
 NDefines.NFrontend.CAMERA_END_Z = 600.0
 NDefines.NFrontend.TIME_FROZEN = 1.0 -- Time before initial animation starts (some delay here so it should NOT be 0, then the animation starts before you can see it)
@@ -20,14 +20,14 @@ NDefines.NFrontend.CAMERA_START_MOVE_TIME = 3.0
 
 NDefines.NFrontend.CAMERA_SPEED_IN_MENUS = 0.1
 
-NDefines.NFrontend.FRONTEND_POS_X = 790.0
+NDefines.NFrontend.FRONTEND_POS_X = 1450.0
 NDefines.NFrontend.FRONTEND_POS_Y = 500.0
 NDefines.NFrontend.FRONTEND_POS_Z = 600.0
-NDefines.NFrontend.FRONTEND_LOOK_X = 790.0
+NDefines.NFrontend.FRONTEND_LOOK_X = 1500.0
 NDefines.NFrontend.FRONTEND_LOOK_Y = 0.0
-NDefines.NFrontend.FRONTEND_LOOK_Z = 1000.0
+NDefines.NFrontend.FRONTEND_LOOK_Z = 1200.0
 
-NDefines.NFrontend.SETTINGS_POS_X = 790.0
+NDefines.NFrontend.SETTINGS_POS_X = 1450.0
 NDefines.NFrontend.SETTINGS_POS_Y = 500.0
 NDefines.NFrontend.SETTINGS_POS_Z = 400.0
 NDefines.NFrontend.SETTINGS_LOOK_X = 1400.0
