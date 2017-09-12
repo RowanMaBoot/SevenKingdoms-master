ec896fe8d4475c2e7a4b95bc61a4489339d59284
Altemio <nobody@rccclan.com>
2017-09-11 19:57:01 -0500

    Damn commas

--- The_Seven_Kingdoms/common/defines/01_camera_reposition.lua
+++ The_Seven_Kingdoms/common/defines/01_camera_reposition.lua
@@ -1,7 +1,7 @@
 NDefines.NFrontend.CAMERA_LOOKAT_X = 790.0 -- Rotation point in main menu
 NDefines.NFrontend.CAMERA_LOOKAT_Y = 0.0
 NDefines.NFrontend.CAMERA_LOOKAT_Z = 1000.0
-NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_X = 1400.0, -- Rotation point in settings
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_X = 1400.0 -- Rotation point in settings
 NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Y = 0.0 -- Y is height
 NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Z = 648.0
 NDefines.NFrontend.CAMERA_START_X = 790.0 -- Initial position in main menu
@@ -13,28 +13,34 @@ NDefines.NFrontend.CAMERA_END_Z = 600.0
 NDefines.NFrontend.TIME_FROZEN = 1.0 -- Time before initial animation starts (some delay here so it should NOT be 0, then the animation starts before you can see it)
 NDefines.NFrontend.GUI_MOVE_SPEED = 800 -- How fast sliding gui objects move ( pixels/s )
 NDefines.NFrontend.MAX_ZOOM_LEVEL = 2500 -- How far out the player can zoom. Warning: At very high zoom levels, the ground will no longer render
+
 NDefines.NFrontend.FADE_IN_DONE_TIME = 2.2
 NDefines.NFrontend.GUI_START_MOVE_TIME = 1.4
 NDefines.NFrontend.CAMERA_START_MOVE_TIME = 3.0
+
 NDefines.NFrontend.CAMERA_SPEED_IN_MENUS = 0.1
+
 NDefines.NFrontend.FRONTEND_POS_X = 790.0
 NDefines.NFrontend.FRONTEND_POS_Y = 500.0
 NDefines.NFrontend.FRONTEND_POS_Z = 600.0
 NDefines.NFrontend.FRONTEND_LOOK_X = 790.0
 NDefines.NFrontend.FRONTEND_LOOK_Y = 0.0
 NDefines.NFrontend.FRONTEND_LOOK_Z = 1000.0
+
 NDefines.NFrontend.SETTINGS_POS_X = 790.0
 NDefines.NFrontend.SETTINGS_POS_Y = 500.0
 NDefines.NFrontend.SETTINGS_POS_Z = 400.0
 NDefines.NFrontend.SETTINGS_LOOK_X = 1400.0
 NDefines.NFrontend.SETTINGS_LOOK_Y = 0.0
 NDefines.NFrontend.SETTINGS_LOOK_Z = 648.0
+
 NDefines.NFrontend.MP_OPTIONS_POS_X = 360.0
 NDefines.NFrontend.MP_OPTIONS_POS_Y = 200.0
 NDefines.NFrontend.MP_OPTIONS_POS_Z = 848.0
 NDefines.NFrontend.MP_OPTIONS_LOOK_X = 490.0
 NDefines.NFrontend.MP_OPTIONS_LOOK_Y = 0.0
 NDefines.NFrontend.MP_OPTIONS_LOOK_Z = 1200.0
+
 NDefines.NFrontend.CREDITS_POS_X = 1000.0
 NDefines.NFrontend.CREDITS_POS_Y = 200.0
 NDefines.NFrontend.CREDITS_POS_Z = 1308.0
