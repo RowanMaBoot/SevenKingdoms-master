46a2feb4071d4c183c19a2c4e5f13d50246279f1
tannerflick4 <tanner.flick4@gmail.com>
2018-09-21 19:36:22 -0400

    braavos barony work & added the on action for the auto_assign minor titles decision as well as some more minor titles added to the decision

---
+++ Personal Folders/tsf4's Palace/minor title list for auto-assign.py
@@ -0,0 +1,30 @@
+callighrer
+qadi
+volva
+drottseti
+skald
+marksman
+hirdman
+sharwarhz
+shahin
+kardarigin
+pushtigbin salar
+spahbod
+eran_ambarag
+councilmember_king
+councilmember_emperor
+teacher
+ser
+king's justice
+knight of the greenhand
+captain of the guard
+leaderminister
+member of parliament
+leaderlord
+lord privy
+lord chamberlain
+lord president
+lord high admiral
+lord freeholder
+high freeholder
+lord dignitary
