0e7a84447f299033e404006729236cc434548164
Vierwood <ethaneeh@hotmail.com>
2019-02-22 12:17:06 -0600

    Menu Camera / Some History

--- The_Seven_Kingdoms/common/defines/01_camera_reposition.lua
+++ The_Seven_Kingdoms/common/defines/01_camera_reposition.lua
@@ -7,7 +7,7 @@ NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Z = 648.0
 NDefines.NFrontend.CAMERA_START_X = 790.0 -- Initial position in main menu
 NDefines.NFrontend.CAMERA_START_Y = 150.0 -- Y is height
 NDefines.NFrontend.CAMERA_START_Z = 500.0
-NDefines.NFrontend.CAMERA_END_X = 1450.0 -- Move to position in main menu
+NDefines.NFrontend.CAMERA_END_X = 1500.0 -- Move to position in main menu
 NDefines.NFrontend.CAMERA_END_Y = 500.0
 NDefines.NFrontend.CAMERA_END_Z = 600.0
 NDefines.NFrontend.TIME_FROZEN = 1.0 -- Time before initial animation starts (some delay here so it should NOT be 0, then the animation starts before you can see it)
@@ -20,26 +20,26 @@ NDefines.NFrontend.CAMERA_START_MOVE_TIME = 3.0
 
 NDefines.NFrontend.CAMERA_SPEED_IN_MENUS = 0.1
 
-NDefines.NFrontend.FRONTEND_POS_X = 1450.0
-NDefines.NFrontend.FRONTEND_POS_Y = 500.0
-NDefines.NFrontend.FRONTEND_POS_Z = 600.0
-NDefines.NFrontend.FRONTEND_LOOK_X = 1500.0
+NDefines.NFrontend.FRONTEND_POS_X = 1000.0
+NDefines.NFrontend.FRONTEND_POS_Y = 350.0
+NDefines.NFrontend.FRONTEND_POS_Z = 750.0
+NDefines.NFrontend.FRONTEND_LOOK_X = 1250.0
 NDefines.NFrontend.FRONTEND_LOOK_Y = 0.0
-NDefines.NFrontend.FRONTEND_LOOK_Z = 1200.0
+NDefines.NFrontend.FRONTEND_LOOK_Z = 1150.0
 
-NDefines.NFrontend.SETTINGS_POS_X = 1450.0
-NDefines.NFrontend.SETTINGS_POS_Y = 500.0
-NDefines.NFrontend.SETTINGS_POS_Z = 400.0
-NDefines.NFrontend.SETTINGS_LOOK_X = 1400.0
+NDefines.NFrontend.SETTINGS_POS_X = 1150.0
+NDefines.NFrontend.SETTINGS_POS_Y = 150.0
+NDefines.NFrontend.SETTINGS_POS_Z = 1300.0
+NDefines.NFrontend.SETTINGS_LOOK_X = 1250.0
 NDefines.NFrontend.SETTINGS_LOOK_Y = 0.0
-NDefines.NFrontend.SETTINGS_LOOK_Z = 648.0
+NDefines.NFrontend.SETTINGS_LOOK_Z = 1500.0
 
-NDefines.NFrontend.MP_OPTIONS_POS_X = 360.0
-NDefines.NFrontend.MP_OPTIONS_POS_Y = 200.0
-NDefines.NFrontend.MP_OPTIONS_POS_Z = 848.0
-NDefines.NFrontend.MP_OPTIONS_LOOK_X = 490.0
+NDefines.NFrontend.MP_OPTIONS_POS_X = 1200.0
+NDefines.NFrontend.MP_OPTIONS_POS_Y = 500.0
+NDefines.NFrontend.MP_OPTIONS_POS_Z = 200.0
+NDefines.NFrontend.MP_OPTIONS_LOOK_X = 1500.0
 NDefines.NFrontend.MP_OPTIONS_LOOK_Y = 0.0
-NDefines.NFrontend.MP_OPTIONS_LOOK_Z = 1200.0
+NDefines.NFrontend.MP_OPTIONS_LOOK_Z = 450.0
 
 NDefines.NFrontend.CREDITS_POS_X = 1000.0
 NDefines.NFrontend.CREDITS_POS_Y = 200.0
